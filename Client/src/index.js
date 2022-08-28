80d517b882d9138f2a8f125e03aca3ca5aa4bb3b
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-08-28 22:12:49 +0530

    dependices added

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import axios from "axios"
+
+ axios.defaults.baseURL = "http://localhost:8080";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <ReduxProvider store={store}> */}
+      <BrowserRouter>
+      <ChakraProvider  >
+        <App />
+      </ChakraProvider>
+      </BrowserRouter>
+    {/* </ReduxProvider> */}
   </React.StrictMode>
 );
 
